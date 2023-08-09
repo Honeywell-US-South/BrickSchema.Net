b894d691b96bfe17f8790f3f6521a28ddf1ae313
Nguyen, Phong (TX95) <phong.nguyen@honeywell.com>
2023-08-09 15:09:09 -0500

    Update behavor to json

--- Helpers/EntityUntils.cs
+++ Helpers/EntityUntils.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,6 +20,32 @@ namespace BrickSchema.Net.Helpers
 
             }
             return tName;
+        }
+
+        public static string BehaviorsToJson(List<BrickBehavior> behaviors)
+        {
+            var settings = new JsonSerializerSettings() { TypeNameHandling = TypeNameHandling.Auto};
+            List<string> behaviorsJson = new();
+            foreach (var b in behaviors)
+            {
+                var bjson = JsonConvert.SerializeObject(b as BrickBehavior, settings);
+                behaviorsJson.Add(bjson);
+            }
+            return JsonConvert.SerializeObject(behaviorsJson, settings);
+        }
+
+        public static List<BrickBehavior> JsonToBehaviors(string json)
+        {
+            if (string.IsNullOrEmpty(json)) return new();
+            List<string>? behaviorsJson = JsonConvert.DeserializeObject<List<string>>(json);
+            List<BrickBehavior> brickBehaviors= new List<BrickBehavior>();
+            foreach (var b in behaviorsJson)
+            {
+                var bb = JsonConvert.DeserializeObject<BrickBehavior>(b);
+                if (bb != null) brickBehaviors.Add(bb);
+            }
+
+            return brickBehaviors;
         }
     }
 }
