e27c5eee5f0eb1aab0d77c847731b967e251260b
Nguyen, Phong (TX95) <phong.nguyen@honeywell.com>
2024-04-08 09:27:18 -0500

    complte adding shape

--- Shapes/Position.cs
+++ Shapes/Position.cs
@@ -2,5 +2,9 @@
 {
     public class Position : BrickShape { }
 
+    public class Latitude : Position { }
+    public class Longitude : Position { }
+    public class Altitude : Position { }
+    public class Heading : Position { }
 
 }
