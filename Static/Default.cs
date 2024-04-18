e4593309e5a4c4a5ed4aec9563afc5035db4dfbe
Nguyen, Phong (TX95) <phong.nguyen@honeywell.com>
2024-04-18 14:31:35 -0500

    add shape to entity dto

---
+++ Static/Default.cs
@@ -0,0 +1,32 @@
+﻿using BrickSchema.Net.Classes.Devices.Vendor.Broadsens.MqttGateway;
+using BrickSchema.Net.EntityProperties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrickSchema.Net.Static
+{
+    public static partial class Default
+    {
+
+        public static List<DefaultAttributeDescription> GetDefaultAttributes(string typeName)
+        {
+            if (typeName.Equals(typeof(Broadsens_GU200S).Name, StringComparison.OrdinalIgnoreCase))
+            {
+                return Broadsens_GU200S_DefaultAttributes();
+            }
+            else if (typeName.Equals(typeof(Broadsens_GU300).Name, StringComparison.OrdinalIgnoreCase))
+            {
+                return Broadsens_GU300_DefaultAttributes();
+            }
+            else if (typeName.Equals(typeof(Broadsens_GU300S).Name, StringComparison.OrdinalIgnoreCase))
+            {
+                return Broadsens_GU300S_DefaultAttributes();
+            }
+            return new();
+        }
+        
+    }
+}
---
+++ Static/DefaultAttributes.cs
@@ -0,0 +1,65 @@
+﻿using BrickSchema.Net.EntityProperties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrickSchema.Net.Static
+{
+    public static partial class Default
+    {
+        private static List<DefaultAttributeDescription> Broadsens_GU200S_DefaultAttributes()
+        {
+            List<DefaultAttributeDescription> defaultProperties = new();
+            defaultProperties.Add(new DefaultAttributeDescription("sensor_type", "Sensor type", "N/A"));
+            defaultProperties.Add(new DefaultAttributeDescription("sensor_id", "Sensor id", "N/A"));
+            defaultProperties.Add(new DefaultAttributeDescription("group_no", "Group Number", "N/A"));
+            defaultProperties.Add(new DefaultAttributeDescription("velox", "Vibration velocity x axis", "mm/s"));
+            defaultProperties.Add(new DefaultAttributeDescription("veloy", " Vibration velocity y axis", "mm/s"));
+            defaultProperties.Add(new DefaultAttributeDescription("veloz", "Vibration velocity z axis", "mm/s"));
+            defaultProperties.Add(new DefaultAttributeDescription("grmsx", " Acceleration rms x axis", "g"));
+            defaultProperties.Add(new DefaultAttributeDescription("grmsy", "Acceleration rms y axis", "g"));
+            defaultProperties.Add(new DefaultAttributeDescription("grmsz", "Acceleration rms z axis", "g"));
+
+
+            return defaultProperties;
+
+        }
+        private static List<DefaultAttributeDescription> Broadsens_GU300_DefaultAttributes()
+        {
+            List<DefaultAttributeDescription> defaultProperties = new();
+            defaultProperties.Add(new DefaultAttributeDescription("sensor_type", "Sensor type", "N/A"));
+            defaultProperties.Add(new DefaultAttributeDescription("sensor_id", "Sensor id", "N/A"));
+            defaultProperties.Add(new DefaultAttributeDescription("group_no", "Group Number", "N/A"));
+            defaultProperties.Add(new DefaultAttributeDescription("velox", "Vibration velocity x axis", "mm/s"));
+            defaultProperties.Add(new DefaultAttributeDescription("veloy", " Vibration velocity y axis", "mm/s"));
+            defaultProperties.Add(new DefaultAttributeDescription("veloz", "Vibration velocity z axis", "mm/s"));
+            defaultProperties.Add(new DefaultAttributeDescription("grmsx", " Acceleration rms x axis", "g"));
+            defaultProperties.Add(new DefaultAttributeDescription("grmsy", "Acceleration rms y axis", "g"));
+            defaultProperties.Add(new DefaultAttributeDescription("grmsz", "Acceleration rms z axis", "g"));
+
+
+            return defaultProperties;
+
+        }
+
+        private static List<DefaultAttributeDescription> Broadsens_GU300S_DefaultAttributes()
+        {
+            List<DefaultAttributeDescription> defaultProperties = new();
+            defaultProperties.Add(new DefaultAttributeDescription("sensor_type", "Sensor type", "N/A"));
+            defaultProperties.Add(new DefaultAttributeDescription("sensor_id", "Sensor id", "N/A"));
+            defaultProperties.Add(new DefaultAttributeDescription("group_no", "Group Number", "N/A"));
+            defaultProperties.Add(new DefaultAttributeDescription("velox", "Vibration velocity x axis", "mm/s"));
+            defaultProperties.Add(new DefaultAttributeDescription("veloy", " Vibration velocity y axis", "mm/s"));
+            defaultProperties.Add(new DefaultAttributeDescription("veloz", "Vibration velocity z axis", "mm/s"));
+            defaultProperties.Add(new DefaultAttributeDescription("grmsx", " Acceleration rms x axis", "g"));
+            defaultProperties.Add(new DefaultAttributeDescription("grmsy", "Acceleration rms y axis", "g"));
+            defaultProperties.Add(new DefaultAttributeDescription("grmsz", "Acceleration rms z axis", "g"));
+
+
+            return defaultProperties;
+
+        }
+    }
+}
