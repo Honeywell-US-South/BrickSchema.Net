77b872318041e253610b599dd82a84f6b9db43c3
Nguyen, Phong (TX95) <phong.nguyen@honeywell.com>
2024-06-25 08:41:34 -0500

    update group

--- Classes/Collections/Groups/UserGroups/UserGroup.cs
+++ Classes/Collections/Groups/UserGroups/UserGroup.cs
@@ -10,5 +10,6 @@ namespace BrickSchema.Net.Classes.Collections.Groups.UserGroups
     public class TenantAdminUserGroup : UserGroup { }
     public class EntityAdminUserGroup : UserGroup { }
     public class EntityUserGroup : UserGroup { }
+    public class EntityUserViewOnlyGroup : UserGroup { }
     public class EntityUserRestrictedGroup : UserGroup { }
 }
